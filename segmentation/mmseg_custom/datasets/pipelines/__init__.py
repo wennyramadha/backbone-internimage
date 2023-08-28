c28e17878cf8a1d705e4bb1bad56368341a9ccf3
MLBRL <mlbrl@MBA13-256G.local>
2023-08-28 16:33:41 +0800

    add config for gta, modify mask2former_head, add mmseg/datasets/gta modify the init, add compose, loading, formatting, test_time_aug, transform to mmseg/datasets/pipeline

--- segmentation/mmseg_custom/datasets/pipelines/__init__.py
+++ segmentation/mmseg_custom/datasets/pipelines/__init__.py
@@ -1,8 +1,18 @@
+# Wenny add compose, loading, test_time_aug
+# ---------------------------------------------------------------------
 # Copyright (c) OpenMMLab. All rights reserved.
-from .formatting import DefaultFormatBundle, ToMask
-from .transform import MapillaryHack, PadShortSide, SETR_Resize
+from .formatting import DefaultFormatBundle, ToMask, ToTensor, Transpose, ToDataContainer, Collect, to_tensor
+from .transform import (MapillaryHack, PadShortSide, SETR_Resize, CLAHE, AdjustGamma, Normalize, Pad,
+                         PhotoMetricDistortion, RandomCrop, RandomFlip,
+                         RandomRotate, Rerange, Resize, RGB2Gray, SegRescale)
+from .compose import Compose
+from .loading import LoadAnnotations, LoadImageFromFile
 
 __all__ = [
-    'DefaultFormatBundle', 'ToMask', 'SETR_Resize',
-    'PadShortSide', 'MapillaryHack'
+    'DefaultFormatBundle', 'ToMask', 'ToTensor', 'Transpose', 'ToDataContainer', 'Collect', 'to_tensor',
+    'SETR_Resize', 'PadShortSide', 'MapillaryHack', 'CLAHE', 'AdjustGamma', 'Normalize', 'Pad',
+    'PhotoMetricDistortion', 'RandomCrop', 'RandomFlip', 'RandomRotate', 'Rerange', 'Resize', 'RGB2Gray',
+    'SegRescale',
+    'Compose',
+    'LoadAnnotations', 'LoadImageFromFile',
 ]
