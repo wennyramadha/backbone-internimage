8c9e30e2d78b89220ce295fc3ec063704490cb65
root <wennyramadha@gmail.com>
2023-08-28 17:26:43 +0800

    modifying in server

--- segmentation/mmseg_custom/datasets/__init__.py
+++ segmentation/mmseg_custom/datasets/__init__.py
@@ -1,10 +1,18 @@
+#Wenny add GTADatasets
+# -------------------------------------------------
 # Copyright (c) OpenMMLab. All rights reserved.
 from .mapillary import MapillaryDataset  # noqa: F401,F403
 from .nyu_depth_v2 import NYUDepthV2Dataset  # noqa: F401,F403
 from .pipelines import *  # noqa: F401,F403
 from .dataset_wrappers import ConcatDataset
+from .cityscapes import CityscapesDataset
+from .gta import GTADataset
 
 
 __all__ = [
-    'MapillaryDataset', 'NYUDepthV2Dataset', 'ConcatDataset'
+    'MapillaryDataset', 
+    'NYUDepthV2Dataset', 
+    'ConcatDataset', 
+    'CityscapesDataset',
+    'GTADataset',
 ]
